232ce25d25def8e800304d82e0a83d362d1cbceb
Shahidul Islam <shahidislamcse123@gmail.com>
2024-05-14 11:48:00 +0600

    First message

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Calculator {
+public class Calculator {
+    public double sum(double a,double b){
+        return a+b;
+    }
+    public double sub(double a,double b){
+        return a-b;
+    }
 }
